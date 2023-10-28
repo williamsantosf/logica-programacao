9056b1a3e0401c4e2a0dd357b0277f265eff39cd
WILLIAM <william.santosf@gmail.com>
2023-10-28 20:56:08 -0300

    Manipulando a Matriz
    Usando métodos para ilustrar manipulação de matrizes.

---
+++ manipulando_matrizes.js
@@ -0,0 +1,14 @@
+let frota = [
+    ["243","RPD-1F94","GABRIEL"],
+    ["244","RPD-8A61","AROLDO"],
+    ["245","RPD-8E24","JOSMAR"]
+    
+   ]
+  
+
+console.log(frota.length)
+console.log("---------------------------------")
+console.log(frota.pop())
+console.log("---------------------------------")
+console.log(frota)
+
