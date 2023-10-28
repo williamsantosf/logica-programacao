b57cb6ab413bf08c152fb5921f85e3280771daa3
WILLIAM <william.santosf@gmail.com>
2023-10-28 20:26:55 -0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let frota243 = ["243","RPD-1F94","GABRIEL"]
+
+console.log(frota243)
+console.log(frota243[0])
+console.log(frota243[1])
+console.log(frota243[2])
+
+
+//arrays são te vetores. Onde recomenda-se guardar dados do mesmo tipo.
---
+++ matrizes.js
@@ -0,0 +1,10 @@
+let frota = [
+    ["243","RPD-1F94","GABRIEL"],
+    ["244","RPD-8A61","AROLDO"],
+    ["245","RPD-8E24","JOSMAR"]
+    
+   ]
+  
+  console.log ("A frota " + frota[0][0] + " é a placa " + frota[0][1] + ", dirigida pelo motorista " + frota[0][2])
+  console.log ("A frota " + frota[1][0] + " é a placa " + frota[1][1] + ", dirigida pelo motorista " + frota[1][2])
+  console.log ("A frota " + frota[2][0] + " é a placa " + frota[2][1] + ", dirigida pelo motorista " + frota[2][2])
